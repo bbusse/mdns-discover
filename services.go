1e1fdb480d95969e5cf22ee7a1c70670cdb703e4
Björn Busse <bj.rn@baerlin.eu>
2023-04-17 23:33:11 +0200

    Add services

--- services.go
+++ services.go
@@ -1,11 +1,19 @@
 package main 
 
 var services = [...]string{
+    "_airplay._tcp",
+    "_dacp._tcp",
+    "_device-info._tcp",
+    "_ftp._tcp",
     "_http._tcp",
     "_raop._tcp",
+    "_rdlink._tcp",
     "_rdp._tcp",
     "_ssh._tcp",
     "_sftp-ssh._tcp",
+    "_sonos._tcp",
+    "_spotify-connect._tcp",
+    "_spotify-social-listening._tcp",
     "_workstation._tcp",
     "_wled._tcp",
 }
